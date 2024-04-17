562060cc02ef18495c65267ead0cdca71bf2dc19
GabeDecian <129911338+GabeDecian@users.noreply.github.com>
2024-04-17 19:25:17 -0300

    Update Test_Runner.c

--- app/test/test_runners/Test_Runner.c
+++ app/test/test_runners/Test_Runner.c
@@ -1,8 +1,8 @@
 #include "unity.h"
 #include "unity_fixture.h"
 
-TEST_GROUP_RUNNER(Foo)
+TEST_GROUP_RUNNER(Sort)
 {
-  RUN_TEST_CASE(Sort, TestQuickSort);
+  RUN_TEST_CASE(Sort, Test_Sort_1);
   //RUN_TEST_CASE(Sort, TestFoo2);
 }
