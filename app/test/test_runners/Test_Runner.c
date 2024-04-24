727552152af82816b7a5768c9ff0b3c0a70e89cd
GabeDecian <gabriel_decian@hotmail.com>
2024-04-23 22:09:51 -0300

    abracadabra

--- app/test/test_runners/Test_Runner.c
+++ app/test/test_runners/Test_Runner.c
@@ -4,5 +4,11 @@
 TEST_GROUP_RUNNER(Sort)
 {
   RUN_TEST_CASE(Sort, Test_Sort_1);
-  //RUN_TEST_CASE(Sort, TestFoo2);
+  RUN_TEST_CASE(Sort, Test_Sort_2);
+  RUN_TEST_CASE(Sort, Test_Sort_3);
+  RUN_TEST_CASE(Sort, Test_Sort_4);
+  RUN_TEST_CASE(Sort, Test_Sort_5);
+  RUN_TEST_CASE(Sort, Test_Sort_6);
+  RUN_TEST_CASE(Sort, Test_Sort_7);
+  RUN_TEST_CASE(Sort, Test_Sort_8);
 }
