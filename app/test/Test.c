4191e5839f7e30919697157cf44df8f9ac852a13
GabeDecian <gabriel_decian@hotmail.com>
2024-05-22 16:39:31 -0300

    Update Test.c

--- app/test/Test.c
+++ app/test/Test.c
@@ -325,7 +325,7 @@ TEST(Sort, Test_Sort_7) //6
 {
   TEST_ASSERT_EQUAL(1, sort(vetor0, 1, On, 6));
   TEST_ASSERT_EQUAL(1, sort(vetor0, 1, On2, 6));
-  TEST_ASSERT_EQUAL(0, sort(vetor0, 1, Onlogn, 6));
+  TEST_ASSERT_EQUAL(1, sort(vetor0, 1, Onlogn, 6));
   TEST_ASSERT_EQUAL(1, sort(vetor1, 24, On, 6));
   TEST_ASSERT_EQUAL(1, sort(vetor1, 24, On2, 6));
   TEST_ASSERT_EQUAL(0, sort(vetor1, 24, Onlogn, 6));
@@ -371,7 +371,7 @@ TEST(Sort, Test_Sort_8) //7
 {
   TEST_ASSERT_EQUAL(1, sort(vetor0, 1, On, 7));
   TEST_ASSERT_EQUAL(1, sort(vetor0, 1, On2, 7));
-  TEST_ASSERT_EQUAL(0, sort(vetor0, 1, Onlogn, 7));
+  TEST_ASSERT_EQUAL(1, sort(vetor0, 1, Onlogn, 7));
   TEST_ASSERT_EQUAL(1, sort(vetor1, 24, On, 7));
   TEST_ASSERT_EQUAL(1, sort(vetor1, 24, On2, 7));
   TEST_ASSERT_EQUAL(0, sort(vetor1, 24, Onlogn, 7));
