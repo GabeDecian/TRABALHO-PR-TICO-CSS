b70dc898226cb66a038b248570cc3c2efeb5e605
GabeDecian <gabriel_decian@hotmail.com>
2024-05-22 16:11:23 -0300

    Update Test.c

--- app/test/Test.c
+++ app/test/Test.c
@@ -47,8 +47,6 @@ int vetor13[20] = {};
 
 TEST(Sort, Test_Sort_1) //0
 {
-  //int simplesVetor[5] = {-1, -2, -3, -4, -5};
-  //TEST_ASSERT_EQUAL(0, sort(simplesVetor, 5, "On", 0)); // Assumindo que '0' é COUNTING_SORT e 'On' é aceito.
   TEST_ASSERT_EQUAL(1, sort(vetor0, 1, On, 0));
   TEST_ASSERT_EQUAL(1, sort(vetor0, 1, On2, 0));
   TEST_ASSERT_EQUAL(1, sort(vetor0, 1, Onlogn, 0));
@@ -95,9 +93,9 @@ TEST(Sort, Test_Sort_1) //0
 
 TEST(Sort, Test_Sort_2) //1
 {
-  //TEST_ASSERT_EQUAL(1, sort(vetor0, 1, On, 1));
-  //TEST_ASSERT_EQUAL(1, sort(vetor0, 1, On2, 1));
-  //TEST_ASSERT_EQUAL(1, sort(vetor0, 1, Onlogn, 1));
+  TEST_ASSERT_EQUAL(1, sort(vetor0, 1, On, 1));
+  TEST_ASSERT_EQUAL(1, sort(vetor0, 1, On2, 1));
+  TEST_ASSERT_EQUAL(1, sort(vetor0, 1, Onlogn, 1));
   ////TEST_ASSERT_EQUAL(1, sort(vetor1, 24, On, 1));
   ////TEST_ASSERT_EQUAL(1, sort(vetor1, 24, On2, 1));
   ////TEST_ASSERT_EQUAL(1, sort(vetor1, 24, Onlogn, 1));
